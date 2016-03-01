20d59a8e1d99b738ef04727133125d2ce00b61d3
Omari Mayers-Walker <omari@fastmail.fm>
2016-02-29 19:23:44 -0500

    completed 2.2.3

--- javascript/2-2-3.js
+++ javascript/2-2-3.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function triangle(length, height) {
+  var length = Number(length);
+  var height = Number(height);
+  var area = (0.5 * length * height);
+  return area;
+}
+
+console.log(triangle(5, 5));
