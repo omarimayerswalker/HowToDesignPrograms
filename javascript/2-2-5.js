e84cc87247f69b48b2b6234a4a636872e01fba98
Omari Mayers-Walker <omari@fastmail.fm>
2016-03-05 17:42:14 -0500

    completed 2.2.5

--- javascript/2-2-5.js
+++ javascript/2-2-5.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function formula1(n) {
+  return (n * n) + 10;
+}
+
+let result1 = formula1(2);
+let result2 = formula1(5);
+let result3 = formula1(9);
+
+console.log('formula1', result1, result2, result3);
+
+function formula2(n) {
+  return (1/2 * (n * n)) + 20;
+}
+
+let result4 = formula2(2);
+let result5 = formula2(5);
+let result6 = formula2(9);
+
+console.log('formula2', result4, result5, result6);
+
+function formula3(n) {
+  return (2 - (1/n)).toFixed(2);
+}
+
+let result7 = formula3(2);
+let result8 = formula3(5);
+let result9 = formula3(9);
+
+console.log('formula3', result7, result8, result9);
