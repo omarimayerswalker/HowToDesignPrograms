d44483cb408480e82605fc2a1a61d4c84f69e67e
Omari Mayers-Walker <omari@fastmail.fm>
2016-03-06 16:05:38 -0500

    completed exercise 2-3-1

---
+++ javascript/lib/2-3-1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+
+function tax(grossPay) {
+  const taxRate = 0.15;
+  return grossPay * taxRate;
+}
+
+function netpay(hours) {
+  const hourlyRate = 12;
+  var gross = hours * hourlyRate;
+
+  return gross - tax(gross);
+}
+
+console.log(netpay(8));
