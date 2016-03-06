9895048940ca915c9daeb3ec03a32f004757215c
Omari Mayers-Walker <omari@fastmail.fm>
2016-03-06 16:18:39 -0500

    completed exercise 2.3.2

---
+++ javascript/lib/2-3-2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function sumCoins(pennies, nickels, dimes, quarters) {
+  const penny = .01;
+  const nickel = .05;
+  const dime = .10;
+  const quarter = .25;
+
+  return ((penny * pennies) + (nickel * nickels) + (dime * dimes) + (quarter * quarters)).toFixed(2);
+}
+
+console.log(sumCoins(1, 1, 1, 1));
